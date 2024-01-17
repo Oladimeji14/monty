3447186b19a6f9b4bebba4a0bf781cdec95980e0
Oladimeji14 <demix1495@gmail.com>
2024-01-17 00:40:26 -0800

    monty.c

---
+++ README.md
@@ -0,0 +1,2 @@
+#include <README.md>
+A collaborate project for a README file of the task 0x19. C - Stacks, Queues - LIFO, FIFO by Salam Qadri and Lateef Adekola
---
+++ monty.c
@@ -0,0 +1,54 @@
+#include "monty.h"
+#include <stdlib.h>
+
+/* Declare op_toks without initialization */
+char **op_toks;
+
+void monty_push(stack_t **stack, unsigned int line_number)
+{
+    char *token;
+    stack_t *new_node;
+
+    token = op_toks[1];
+
+    if (!token || atoi(token) == 0)
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        set_op_tok_error(EXIT_FAILURE);
+        return;
+    }
+
+    new_node = malloc(sizeof(stack_t));
+
+    if (new_node == NULL)
+    {
+        malloc_error();
+        return;
+    }
+
+    new_node->n = atoi(token);
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    if (*stack != NULL)
+    {
+        (*stack)->prev = new_node;
+    }
+
+    *stack = new_node;
+}
+
+void monty_pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *current;
+
+    (void)line_number;
+
+    current = *stack;
+
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
