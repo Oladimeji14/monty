d5d1a85924ddfca5fbcb5cfae8cb96d8d9eade82
stonecode6483 <lateefadekola90@gmail.com>
2024-01-18 03:22:01 -0800

    monty

--- monty.c
+++ monty.c
@@ -3,52 +3,62 @@
 
 /* Declare op_toks without initialization */
 char **op_toks;
-
+/**
+ * monty_push - Pushes an element onto the stack.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Line number of the current operation
+ */
 void monty_push(stack_t **stack, unsigned int line_number)
 {
-    char *token;
-    stack_t *new_node;
+	char *token;
+	stack_t *new_node;
 
-    token = op_toks[1];
+	token = op_toks[1];
 
-    if (!token || atoi(token) == 0)
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        set_op_tok_error(EXIT_FAILURE);
-        return;
-    }
+	if (!token || atoi(token) == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		set_op_tok_error(EXIT_FAILURE);
+		return;
+	}
 
-    new_node = malloc(sizeof(stack_t));
+	new_node = malloc(sizeof(stack_t));
 
-    if (new_node == NULL)
-    {
-        malloc_error();
-        return;
-    }
+	if (new_node == NULL)
+	{
+		malloc_error();
+		return;
+	}
 
-    new_node->n = atoi(token);
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	new_node->n = atoi(token);
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    if (*stack != NULL)
-    {
-        (*stack)->prev = new_node;
-    }
+	if (*stack != NULL)
 
-    *stack = new_node;
+	{
+		(*stack)->prev = new_node;
+	}
+
+	*stack = new_node;
 }
+/**
+ *monty_pall - Prints all elements of the stack.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Line number of the current operation.
+ */
 
 void monty_pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current;
+	stack_t *current;
 
-    (void)line_number;
+	(void)line_number;
 
-    current = *stack;
+	current = *stack;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
